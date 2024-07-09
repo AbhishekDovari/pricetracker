12fd6fbabcc3aa988b1eeb92c66649d09c6e7f67
AbhishekDovari <abhidovari@gmail.com>
2024-07-09 10:56:01 +0530

    fixed product error

--- lib/actions/index.ts
+++ lib/actions/index.ts
@@ -1,117 +1,120 @@
 "use server"
-import { revalidatePath } from "next/cache";
-import { connect } from "http2";
+
 import { scrapedAmazonProduct } from "../scraper";
 import { connectToDB } from "../scraper/mongoose";
+import { generateEmailContent, sendEmail } from "../nodemailer";
+import { revalidatePath } from "next/cache";
 import Product from "../models/product.model";
 import { getAveragePrice, getHighestPrice, getLowestPrice } from "../utils";
 import { User } from "@/types";
-import { generateEmailContent, sendEmail } from "../nodemailer";
 
-export async function scrapeAndStoreProductData(url: string) {
-    if(!url) return;
+export async function scrapeAndStoreProduct(productUrl: string) {
+  if(!productUrl) return;
 
-    try {
-        connectToDB();
+  try {
+    connectToDB();
 
-        const scrapedProduct = await scrapedAmazonProduct(url);
+    const scrapedProduct = await scrapedAmazonProduct(productUrl);
 
-        let product = scrapedProduct;
+    if(!scrapedProduct) return;
 
-        const existingProduct = await Product.findOne({ url: scrapedProduct?.url });
+    let product = scrapedProduct;
 
+    const existingProduct = await Product.findOne({ url: scrapedProduct.url });
 
-        if(existingProduct) {
-            const updatedPriceHistory: any = [
-                ...existingProduct.priceHistory,
-                { price: scrapedProduct?.currentPrice }
-            ]
-            
-            product={
-                ...scrapedProduct,
-                priceHistory: updatedPriceHistory,
-                lowestPrice: getLowestPrice(updatedPriceHistory),
-                highestPrice: getHighestPrice(updatedPriceHistory),
-                averagePrice: getAveragePrice(updatedPriceHistory),
-                // discountRate: calculateDiscountRate(scrapedProduct.originalPrice, scrapedProduct.currentPrice),
-            }
-        }
-        const newProduct = await Product.findOneAndUpdate(
-            { url: scrapedProduct?.url },
-            product,
-            {upsert: true, new: true }
-        );
+    if(existingProduct) {
+      const updatedPriceHistory: any = [
+        ...existingProduct.priceHistory,
+        { price: scrapedProduct.currentPrice }
+      ]
 
-        revalidatePath(`/products/$(newProduct._id)`);
-        
-
-    } catch (error :any) {
-        throw new Error(`Failed to scrape product data: ${error.message}`);
-        
+      product = {
+        ...scrapedProduct,
+        priceHistory: updatedPriceHistory,
+        lowestPrice: getLowestPrice(updatedPriceHistory),
+        highestPrice: getHighestPrice(updatedPriceHistory),
+        averagePrice: getAveragePrice(updatedPriceHistory),
+      }
     }
+
+    const newProduct = await Product.findOneAndUpdate(
+      { url: scrapedProduct.url },
+      product,
+      { upsert: true, new: true }
+    );
+
+    revalidatePath(`/products/${newProduct._id}`);
+  } catch (error: any) {
+    throw new Error(`Failed to create/update product: ${error.message}`)
+  }
 }
 
-export async function getProductById(id: string) {
-    
-    try {
-        connectToDB();
-        const product = await Product.findOne({ _id: id });
+export async function getProductById(productId: string) {
+  try {
+    connectToDB();
 
-        if(!product) {
-            // throw new Error('Product not found');
-            return null;
-        }
-        return product;
+    const product = await Product.findOne({ _id: productId });
 
-    } catch (error :any) {
-        // throw new Error(`Failed to get product by id: ${error.message}`);
-        console.log(error);
-    }
+    if(!product) return null;
 
-
-
+    return product;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export async function getAllProducts() {
-    try {
-        connectToDB();
-        const products = await Product.find();
-        return products;
-    } catch (error) {
-        console.log(error);
-    }
+  try {
+    connectToDB();
+
+    const products = await Product.find();
+
+    return products;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function getSimilarProducts(productId : string) {
-    try {
-        connectToDB();
-        const currentPruduct = await Product.findOne({ productId });
-        if(!productId) return null;
-        const similarProducts = await Product.find({
-            id: { $ne: productId },
-        }).limit(4);
-        return similarProducts;
-    } catch (error) {
-        console.log(error);
-    }
+export async function getSimilarProducts(productId: string) {
+  try {
+    connectToDB();
+
+    const currentProduct = await Product.findById(productId);
+
+    if(!currentProduct) return null;
+
+    const similarProducts = await Product.find({
+      _id: { $ne: productId },
+    }).limit(3);
+
+    return similarProducts;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-export async function addUserEmailToProduct(productId: string, email: string) {
-    try {
-        const product = await Product.findById(productId);
-        if(!product) return;
-        
-        const userExists = product.users.some((user: User) => user.email === email);
-        if(!userExists) {
-            product.users.push({ email: email });
-            await product.save();
+export async function addUserEmailToProduct(productId: string, userEmail: string) {
+  try {
+    const product = await Product.findById(productId);
 
-            const emailContent = await generateEmailContent(product, "WELCOME");
-            await sendEmail(emailContent, [email]);
-        }
+    if(!product) return;
 
+    const userExists = product.users.some((user: User) => user.email === userEmail);
+
+    if(!userExists) {
+      product.users.push({ email: userEmail });
+
+      await product.save();
+
+      const emailContent = await generateEmailContent(product, "WELCOME");
+
+      await sendEmail(emailContent, [userEmail]);
     }
-    catch (error) {
-        console.log(error);
-    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function generateEmailBody(product: any, arg1: string) {
+    throw new Error("Function not implemented.");
 }
